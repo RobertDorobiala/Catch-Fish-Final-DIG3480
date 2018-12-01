9e91be7897dcc4f0445461dcf582667bd0b82b09
RobertDorobiala <42683580+RobertDorobiala@users.noreply.github.com>
2018-12-01 15:12:19 -0500

    Final

---
+++ FUNTIMES/Assets/RoDoScripts/RoDoPlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoDoPlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private bool facingRight = true;
+    public float speed;
+
+    // Use this for initialization
+    void Start ()
+    { 
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+
+        if (facingRight == false && moveHorizontal > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveHorizontal < 0)
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+}
